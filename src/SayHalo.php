872230757aa306abd3f2443dd5fb0bfed4dc22c8
Ratno Putro Sulistiyono <ratno@knoqdown.com>
2015-06-20 13:36:08 +0700

    inisiasi

---
+++ src/SayHalo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Belajar;
+
+class SayHalo
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Belajar\SayHalo;
+
+echo SayHalo::world();
